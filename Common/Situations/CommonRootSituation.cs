928f939c933c8fd23bc56b00ce154cd229630d53
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-09-10 22:27:39 -0500

    Add common root situation error handling

---
+++ Common/Situations/CommonRootSituation.cs
@@ -0,0 +1,30 @@
+﻿namespace Gamefreak130.Common.Situations
+{
+    using Sims3.Gameplay.Autonomy;
+    using Sims3.Gameplay.Core;
+
+    public abstract class CommonRootSituation : RootSituation
+    {
+        public CommonRootSituation()
+        {
+        }
+
+        public CommonRootSituation(Lot lot) : base(lot) 
+            => Initialize(null);
+
+        public override void Initialize(Situation _)
+        {
+            try
+            {
+                Init();
+            }
+            catch
+            {
+                Exit();
+                throw;
+            }
+        }
+
+        protected abstract void Init();
+    }
+}
