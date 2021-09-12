e6afc53cc10f24d64525c7e9ae3e544cfa7c87fe
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2021-09-11 22:06:46 -0500

    Add common delegate error handling

---
+++ Common/Listeners/EventHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Gamefreak130.Common.Listeners
+{
+    using Sims3.Gameplay.Abstracts;
+    using Sims3.Gameplay.Actors;
+    using Sims3.Gameplay.CAS;
+    using Sims3.Gameplay.EventSystem;
+
+    public static class EventHelper
+    {
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, null, null, exceptionAction);
+
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, GameObject target, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, null, target, exceptionAction);
+
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, Sim actor, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, actor, null, exceptionAction);
+
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, Household actorHousehold, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, actorHousehold, null, exceptionAction);
+
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, Sim actor, GameObject target, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, actor, target, exceptionAction);
+
+        public static EventListener AddDelegateListener(EventTypeId id, ProcessEventDelegate @delegate, Household actorHousehold, GameObject target, ListenerAction exceptionAction = ListenerAction.Remove)
+            => AddDelegateListenerInternal(id, @delegate, actorHousehold, target, exceptionAction);
+
+        private static EventListener AddDelegateListenerInternal(EventTypeId id, ProcessEventDelegate @delegate, ScriptObject s, GameObject target, ListenerAction exceptionAction) 
+            => EventTracker.AddListener(new DelegateListener(id, SafeProcessEventDelegate.Create(@delegate, exceptionAction), s, target));
+    }
+}
---
+++ Common/Listeners/SafeProcessEventDelegate.cs
@@ -0,0 +1,41 @@
+﻿namespace Gamefreak130.Common.Listeners
+{
+    using Gamefreak130.Common.Loggers;
+    using Sims3.Gameplay.EventSystem;
+    using System;
+
+    public class SafeProcessEventDelegate
+    {
+        private readonly ProcessEventDelegate mOriginalDelegate;
+
+        private readonly ListenerAction mExceptionAction;
+
+        private SafeProcessEventDelegate(ProcessEventDelegate originalDelegate, ListenerAction exceptionAction)
+        {
+            mOriginalDelegate = originalDelegate;
+            mExceptionAction = exceptionAction;
+        }
+
+        private ListenerAction ProcessEvent(Event e)
+        {
+            try
+            {
+                return mOriginalDelegate(e);
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogger.sInstance.Log(ex);
+
+                if (mExceptionAction is ListenerAction.Remove)
+                {
+                    // Rethrow to the EventTracker so that the listener is removed without setting CompletionEvent
+                    throw;
+                }
+                return ListenerAction.Keep;
+            }
+        }
+
+        public static ProcessEventDelegate Create(ProcessEventDelegate @delegate, ListenerAction exceptionAction)
+            => new SafeProcessEventDelegate(@delegate, exceptionAction).ProcessEvent;
+    }
+}
